ac8af7431afcb4ddb77655100fc7433100378ba0
rmaWhrdl <wwscan3@gmail.com>
2023-08-30 23:48:29 +0900

    <서블릿> HTTP 응답 데이터 - API JSON
    - HTTP 응답으로 JSON을 반환할 때는 Content-Type을 application/json 으로 지정해야 한다.
    
    - Jackson 라이브러리가 제공하는 objectMapper.writeValueAsString()을 사용하면 객체를 JSON 문자로 변경할 수 있다.
    
    - 참고
       * application/json은 스펙상 utf-8형식을 사용하도록 정의되어 있다. (따라서 따로 추가하지 않아도 된다.)
       * response.getWriter()를 사용하면 추가 파라미터를 자동으로 추가해버린다. 이때는 response.getOutputStream()으로 출력하면 그런 문제가 없다.

---
+++ src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,34 @@
+package hello.servlet.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Content-Type: application/json
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        HelloData helloData = new HelloData();
+        helloData.setUsername("jangmi");
+        helloData.setAge(23);
+
+        // JSON 형식으로 변환 및 출력 {"username":"jangmi", "age":23}
+        String result = objectMapper.writeValueAsString(helloData);
+        PrintWriter writer = response.getWriter();
+        writer.println(result);
+    }
+}
