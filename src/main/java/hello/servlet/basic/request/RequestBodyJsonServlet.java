a504e87d82e23c4a6fef71fe9d6f181908807398
rmaWhrdl <wwscan3@gmail.com>
2023-08-30 22:28:35 +0900

    <서블릿> HTTP 요청 데이터 - API 메시지 바디(JSON)
    - 스프링 부트에서 지원하는 JSON 변환 라이브러리인 Jackson 라이브러리를 사용한다.
    - Jackson 라이브러리를 사용해서 요청 메세지의 Body 부분을 읽어와서 String 파일을 JSON 파일로 변환해준다.
    
    *참고
       - HTML form 데이터도 Message Body를 통해 전송되므로 직접 읽을 수 있다.
       - 하지만 request.getParameter()와 같이 편리하게 Parameter를 조회할 수 있기 때문에 이 기능을 사용한다.

---
+++ src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
@@ -0,0 +1,36 @@
+package hello.servlet.basic.request;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.util.StreamUtils;
+import java.io.IOException;
+import static java.nio.charset.StandardCharsets.*;
+
+@WebServlet(name = "requestBodyJsonServlet", urlPatterns = "/request-body-json")
+public class RequestBodyJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, UTF_8);
+
+        System.out.println("messageBody = " + messageBody);
+
+        /*
+          스프링 부트로 Spring MVC 를 선택하면 기본으로 Jackson 라이브러리(objectMapper)를 함께 제공한다.
+          Jackson 라이브러리는 JSON 변환 라이브러리이다.
+         */
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+
+        System.out.println("helloData.username = " + helloData.getUsername());
+        System.out.println("helloData.age = " + helloData.getAge());
+    }
+}
